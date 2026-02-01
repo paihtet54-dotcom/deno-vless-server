af86f3a0f23ff2df27f17eff331d607c21b68e81
paihtet54-dotcom <paihtet.54@gmail.com>
2026-02-01 08:15:08 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '9048b618-a2ea-495e-85fa-88e1405b833f'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
